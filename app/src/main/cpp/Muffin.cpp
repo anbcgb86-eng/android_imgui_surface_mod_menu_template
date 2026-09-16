47a9c088dba2831c5c0d1d82799851d40f2effc4
anbcgb86-eng <anbcgb86@gmail.com>
2026-09-16 08:22:09 +0700

    Update Muffin.cp

--- app/src/main/cpp/Muffin.cpp
+++ app/src/main/cpp/Muffin.cpp
@@ -1,14 +1,8 @@
-#include <jni.h>
-#include <string>
-#include <thread>
-#include <android/log.h>
-#include "Includes.h"
+#include "imgui.h"
 
-// Hàm vẽ giao diện chính của Mod Menu
-void DrawMenu() {
+void Main() {
     ImGui::Begin("Mod Menu VIP - FF");
 
-    // Các Tab chức năng
     static int tab = 0;
     if (ImGui::Button("Combat", ImVec2(90, 35))) tab = 0; 
     ImGui::SameLine();
@@ -16,7 +10,6 @@ void DrawMenu() {
 
     ImGui::Separator();
 
-    // Tab 1: Combat
     if (tab == 0) {
         static bool aimbot = false;
         static bool aimHeadDrag = false;
@@ -26,7 +19,6 @@ void DrawMenu() {
             ImGui::Checkbox("Aim Head Drag (Kéo tâm vào đầu)", &aimHeadDrag);
         }
     } 
-    // Tab 2: Visual
     else if (tab == 1) {
         static bool espBox = false;
         ImGui::Checkbox("Vẽ hộp ESP", &espBox);
